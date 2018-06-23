3d7a09ec9ee851d0d0b1561d803578de6f7d1c6e
MohamedBakrAli <mohamed556d@gmail.com>
2018-06-23 17:28:21 +0200

    continue lesson 6

--- SQL_Aggregations_lesson_6.sql
+++ SQL_Aggregations_lesson_6.sql
@@ -95,3 +95,178 @@ SELECT r.name , count(*) cnt
 FROM region r JOIN sales_reps s ON (r.id = s.region_id)
 GROUP BY r.name
 ORDER BY cnt;
+
+						--Quiz: GROUP BY Part || --
+					   --------------------
+-- prob 1
+SELECT a.name, AVG(o.standard_qty) avg_standard_qty,AVG(o.gloss_qty) avg_gloss_qty, AVG(o.poster_qty) avg_poster_qty
+FROM accounts a JOIN orders o ON a.id = o.account_id
+GROUP BY a.name
+
+-- prob 2
+SELECT a.name, AVG(o.standard_amt_usd ) avg_standard_amt_usd ,
+AVG(o.gloss_amt_usd ) avg_gloss_amt_usd, AVG(o.poster_amt_usd ) avg_poster_amt_usd 
+FROM accounts a JOIN orders o ON a.id = o.account_id
+GROUP BY a.name
+
+-- prob 3
+SELECT s.name, w.channel, COUNT(*) occurrences
+FROM accounts a JOIN sales_reps s ON a.sales_rep_id = s.id JOIN web_events w ON a.id = w.account_id
+GROUP BY s.name, w.channel
+ORDER BY occurrences DESC;
+
+-- prob 4
+SELECT r.name, w.channel, COUNT(*) occurrences
+FROM accounts a JOIN sales_reps s ON a.sales_rep_id = s.id JOIN web_events w ON a.id = w.account_id
+JOIN region r ON r.id = s.region_id
+GROUP BY r.name, w.channel
+ORDER BY occurrences DESC;
+
+						--Quiz: DISTINCT--
+					   --------------------
+-- prob 1
+SELECT DISTINCT a.id account_id, r.id region_id
+FROM accounts a JOIN sales_reps s ON a.sales_rep_id = s.id JOIN region r ON r.id = s.region_id;
+
+SELECT DISTINCT id, name 
+FROM accounts;
+
+-- prob 2
+SELECT s.id, s.name, COUNT(*) num_accounts
+FROM accounts a
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY s.id, s.name
+ORDER BY num_accounts;
+
+
+SELECT id 
+FROM Sales_reps;
+
+						--Quiz: HAVING--
+					   --------------------
+-- prob 1
+SELECT s.id, s.name, COUNT(*) num_accounts
+FROM accounts a
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY s.id, s.name
+HAVING COUNT(*) > 5
+ORDER BY num_accounts;
+
+-- prob 2
+SELECT a.id, a.name, COUNT(*) num_orders
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+HAVING COUNT(*) > 20
+ORDER BY num_orders;
+
+-- prob 3
+SELECT a.id, a.name, COUNT(*) num_orders
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+ORDER BY num_orders DESC 
+LIMIT 1;
+
+-- prob 4
+SELECT a.id, a.name, SUM(total_amt_usd) total_amt_usd
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+HAVING SUM(total_amt_usd) > 30000
+ORDER BY total_amt_usd DESC;
+
+-- prob 5
+SELECT a.id, a.name, SUM(total_amt_usd) total_amt_usd
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+HAVING SUM(total_amt_usd) < 1000
+ORDER BY total_amt_usd DESC;
+
+-- prob 6
+SELECT a.id, a.name, SUM(total_amt_usd) total_amt_usd
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+ORDER BY total_amt_usd DESC
+LIMIT 1;
+
+-- prob 7
+SELECT a.id, a.name, SUM(total_amt_usd) total_amt_usd
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+ORDER BY total_amt_usd
+LIMIT 1;
+
+-- prob 8
+SELECT a.id, a.name, COUNT(*) cnt
+FROM accounts a 
+JOIN web_events w ON a.id = w.account_id
+GROUP BY a.id, a.name
+HAVING COUNT(*) > 6 AND w.channel = 'facebook'
+ORDER BY cnt;
+
+-- prob 9
+SELECT a.id, a.name, COUNT(*) cnt
+FROM accounts a 
+JOIN web_events w ON a.id = w.account_id
+WHERE w.channel = 'facebook'
+GROUP BY a.id, a.name
+HAVING COUNT(*) > 6
+ORDER BY cnt DESC
+LIMIT 1;
+
+-- prob 10
+SELECT w.channel, COUNT(*) cnt 
+FROM accounts a 
+JOIN web_events w ON a.id = w.account_id
+GROUP BY w.channel
+ORDER BY cnt DESC
+LIMIT 1;
+
+						--Quiz: DATE functions--
+					   --------------------
+-- prob 1
+SELECT DATE_TRUNC('year', occurred_at) year_date, SUM(total_amt_usd)
+FROM orders
+GROUP BY 1
+ORDER BY 2 DESC;
+
+-- prob 2
+SELECT DATE_PART('month', occurred_at) month_date, SUM(total_amt_usd) total_spent
+FROM orders
+WHERE occurred_at BETWEEN '2014-01-01' AND '2017-01-01'
+GROUP BY 1
+ORDER BY 2 DESC;
+
+-- prob 3
+SELECT DATE_PART('year', occurred_at) year_date, SUM(total_amt_usd) total_spent
+FROM orders
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
+
+-- prob 4
+SELECT DATE_PART('month', occurred_at) month_date, COUNT(*) cnt
+FROM orders
+WHERE occurred_at BETWEEN '2014-01-01' AND '2017-01-01'
+GROUP BY 1
+ORDER BY 2 DESC;
+
+-- prob 5
+SELECT DATE_PART('month', occurred_at) month_date, SUM(o.gloss_amt_usd) total
+FROM orders o JOIN accounts a ON a.id = o.account_id
+WHERE a.name = 'Walmart'
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
